d80fbe4aee9b552817c1697ae5ffd1c0e3da61ec
Victoria Petina <kira.abramovich@gmail.com>
2022-02-05 13:47:35 +0300

    change code up to instructions

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-let randomNumber1 = Math.floor((Math.random() * 6)) + 1,
-    randomNumber2 = Math.floor((Math.random() * 6)) + 1,
+let randomNumber1 = Math.floor(Math.random() * 6) + 1,
+    randomNumber2 = Math.floor(Math.random() * 6) + 1,
     firstPlayer = document.querySelector('.img1'),
     secondPlayer = document.querySelector('.img2'),
     playTitle = document.querySelector("h1");
 
-
-    firstPlayer.src = "/images/dice" + randomNumber1 + ".png";
-    secondPlayer.src = "/images/dice" + randomNumber2 + ".png";
+    firstPlayer.setAttribute("src", "/images/dice" + randomNumber1 + ".png");
+    secondPlayer.setAttribute("src", "/images/dice" + randomNumber2 + ".png");
 
     if (randomNumber1 === randomNumber2) {
         playTitle.textContent = 'Draw!';
     } else if (randomNumber1 > randomNumber2) {
-        playTitle.textContent = 'Player 1 Wins!';
+        playTitle.textContent = '🚩Player 1 Wins!';
     } else {
-        playTitle.textContent = 'Player 2 Wins!';
+        playTitle.textContent = 'Player 2 Wins!🚩';
     }
